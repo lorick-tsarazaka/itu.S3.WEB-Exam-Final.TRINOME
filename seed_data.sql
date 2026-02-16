f3db24a154214f5d1bab03bab962edf4972a9dfc
Lorick TSARAZAKA <lorick.tsarazaka.lk@gmail.com>
2026-02-16 21:49:08 +0300

    Update seed_data.sql

--- seed_data.sql
+++ seed_data.sql
@@ -77,28 +77,34 @@ INSERT INTO bngrc_besoin (b_id, b_libelle, b_prixUnitraire, b_categorie, b_unite
 (10,'Filets anti-moustiques', 7.50, 3, 1),
 (11,'Vêtements', 4.00, 7, 1);
 
+-- Status values for besoinVille
+INSERT INTO bngrc_statusBesoinVille (sbv_id, sbv_libelle) VALUES
+(1, 'attendu'),
+(2, 'partiel'),
+(3, 'couvert');
+
 -- Initial stock needs per city (besoinVille)
-INSERT INTO bngrc_besoinVille (bv_id, bv_besoin, bv_quantite, bv_ville) VALUES
-(1, 1, 5000, 1), -- Antananarivo: eau
-(2, 2, 2000, 1), -- riz
-(3, 3, 800, 1),
-(4, 4, 150, 1),
-(5, 5, 300, 1),
-(6, 6, 400, 1),
-(7, 7, 100, 1),
-(8, 8, 200, 1),
-(9, 9, 250, 1),
-(10,10, 500, 1),
-(11, 1, 2000, 4), -- Toamasina (cyclone)
-(12, 2, 800, 4),
-(13, 3, 300, 4),
-(14, 4, 100, 4),
-(15, 5, 120, 4),
-(16, 6, 150, 4),
-(17, 1, 1200, 10), -- Farafangana
-(18, 2, 600, 10),
-(19, 5, 80, 3), -- Fianarantsoa
-(20, 1, 700, 3);
+INSERT INTO bngrc_besoinVille (bv_id, bv_besoin, bv_quantite, bv_ville, bv_status) VALUES
+(1, 1, 5000, 1, 2), -- Antananarivo: partiel
+(2, 2, 2000, 1, 2), -- partiel
+(3, 3, 800, 1, 2),
+(4, 4, 150, 1, 2),
+(5, 5, 300, 1, 2),
+(6, 6, 400, 1, 2),
+(7, 7, 100, 1, 2),
+(8, 8, 200, 1, 2),
+(9, 9, 250, 1, 2),
+(10,10, 500, 1, 2),
+(11, 1, 2000, 4, 1), -- Toamasina (cyclone): attendu
+(12, 2, 800, 4, 1),
+(13, 3, 300, 4, 1),
+(14, 4, 100, 4, 1),
+(15, 5, 120, 4, 1),
+(16, 6, 150, 4, 1),
+(17, 1, 1200, 10, 2), -- Farafangana: partiel
+(18, 2, 600, 10, 2),
+(19, 5, 80, 3, 1), -- Fianarantsoa: attendu
+(20, 1, 700, 3, 1);
 
 -- Exemple de collectes
 INSERT INTO bngrc_collecte (c_id, c_date) VALUES
