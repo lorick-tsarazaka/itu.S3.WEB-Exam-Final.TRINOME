f1ef4553cdec85c81fd54f866b1e62aaf063eb5a
CHARLES <amelsoanasycharles@gmail.com>
2026-02-16 15:03:14 +0300

    create repository file for Besoin and Distribution

---
+++ app/repositories/BesoinRepotisory.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\repositories;
+
+use PDO;
+
+class BesoinRepository {
+    private PDO $pdo;
+
+    public function __construct(PDO $pdo) {
+        $this->pdo = $pdo;
+    }
+}
