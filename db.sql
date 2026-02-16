07fbd26fdcb80e7e521878251230aad598d5476d
rivaldjr39 <rivaldoako25@gmail.com>
2026-02-16 14:51:05 +0300

    base2-16/02/26

--- db.sql
+++ db.sql
@@ -24,3 +24,23 @@ create table bngrc_regionVille (
   foreign key (rv_ville) references bngrc_ville(v_id)
 );
 
+-- Table bngrc_sinistre
+create table bngrc_sinistre (
+  s_id int primary key auto_increment,
+  s_nom varchar(255) not null,
+  s_ville int not null,
+  foreign key (s_ville) references bngrc_ville(v_id)
+);
+
+-- Table bngrc_categorieBesoin
+create table bngrc_categorieBesoin (
+  cb_id int primary key auto_increment,
+  cb_libelle varchar(255) not null
+);
+
+-- Table bngrc_uniteBesoin
+create table bngrc_uniteBesoin (
+  ub_id int primary key auto_increment,
+  ub_libelle varchar(255) not null
+);
+
