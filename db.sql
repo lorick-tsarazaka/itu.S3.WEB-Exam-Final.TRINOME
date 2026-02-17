5156f30fe275829467f4e2be2818b283ae3fb945
Lorick TSARAZAKA <lorick.tsarazaka.lk@gmail.com>
2026-02-17 09:39:28 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -5,9 +5,9 @@ use final_exam_s3;
  drop TABLE IF EXISTS bngrc_regionVille;
  drop TABLE IF EXISTS bngrc_besoinVille;
  drop TABLE IF EXISTS bngrc_sinistre;
- drop TABLE IF EXISTS bngrc_besoin;
  drop TABLE IF EXISTS bngrc_categorieBesoin;
  drop TABLE IF EXISTS bngrc_uniteBesoin;
+ drop TABLE IF EXISTS bngrc_besoin;
  drop TABLE IF EXISTS bngrc_statusBesoinVille;
  drop TABLE IF EXISTS bngrc_collecteDetails;
  drop TABLE IF EXISTS bngrc_collecte;
@@ -79,7 +79,7 @@ create table bngrc_besoinVille (
   bv_id int primary key auto_increment,
   bv_besoin int not null,
   bv_quantite int not null,
-  bv_date_demande date not null default CURRENT_DATE,
+  bv_date_demande date,
   bv_ville int not null,
   bv_status int not null,
   foreign key (bv_besoin) references bngrc_besoin(b_id),
